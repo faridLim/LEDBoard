005512312e74e064a87216efa677ac2e8d6ee3fe
faridLim <gjsl1457@gmail.com>
2021-10-01 15:46:11 +0900

    Sending data using delegate protocol

--- LEDBOARD/SettingViewController.swift
+++ LEDBOARD/SettingViewController.swift
@@ -1,16 +1,20 @@
 //
 //  SettingViewController.swift
 //  LEDBOARD
-//
+//변경하려는 색상 값에 대한 데이터를 ViewController로 전달하기위해 delegate패턴사용
 //  Created by 임재헌 on 2021/09/28.
 //
 
 import UIKit
 
+protocol LEDBoardSettingDelegate : AnyObject{
+    func changedSetting(text : String?, textColor : UIColor, backgroundColor : UIColor)
+}
+
+
 class SettingViewController: UIViewController {
 
     @IBOutlet weak var textField: UITextField!
-    
     
     @IBOutlet weak var yellowBtn: UIButton!
     @IBOutlet weak var purpleBtn: UIButton!
@@ -18,6 +22,10 @@ class SettingViewController: UIViewController {
     @IBOutlet weak var blackBtn: UIButton!
     @IBOutlet weak var blueBtn: UIButton!
     @IBOutlet weak var orangeBtn: UIButton!
+    
+    weak var delegate : LEDBoardSettingDelegate?
+    var textColor : UIColor = .yellow  //초기 ViewController에 나타난 색상으로 초기값 설정
+    var backgroundColor : UIColor = .black
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,24 +37,33 @@ class SettingViewController: UIViewController {
         //사용자가 어떤 버튼을 클릭횄는지 표시되도록 선택된 버튼의 알파값을 1로, 선택되지 않은 버튼의 알파값을 0.2로 변경
         if sender == self.yellowBtn{
             self.changeTextColorBtn(color: .yellow)
+            self.textColor = .yellow
         } else if sender == self.purpleBtn{
             self.changeTextColorBtn(color: .purple)
+            self.textColor = .purple
         } else {
             self.changeTextColorBtn(color: .green)
+            self.textColor = .green
         }
 
     }
     @IBAction func tabBackgroundColorBtn(_ sender: UIButton) {
         if sender == self.blackBtn {
             self.changeBackgroundColorBtn(color: .black)
+            self.backgroundColor = .black
         } else if sender == self.blueBtn{
             self.changeBackgroundColorBtn(color: .blue)
+            self.backgroundColor = .blue
         } else {
             self.changeBackgroundColorBtn(color: .orange)
+            self.backgroundColor = .orange
         }
     }
     
     @IBAction func tabSaveBtn(_ sender: UIButton) {
+        self.delegate?.changedSetting(text: self.textField.text,
+                                      textColor: self.textColor,
+                                      backgroundColor: self.backgroundColor)
     }
     private func changeTextColorBtn(color : UIColor){
         //parameter로 전달받은 color가 'UIColor.yellow'에 해당하면 self.yellowBtn에 1을 아니면 0.2를 할당
--- LEDBOARD/ViewController.swift
+++ LEDBOARD/ViewController.swift
@@ -7,15 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,LEDBoardSettingDelegate {
     @IBOutlet weak var contentsLabel: UILabel!
     
+    func changedSetting(text: String?, textColor: UIColor, backgroundColor: UIColor) {
+        if  text != nil{
+            self.contentsLabel.text = text
+        }
+        self.contentsLabel.textColor = textColor
+        self.view.backgroundColor = backgroundColor
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         self.contentsLabel.textColor = .yellow
         
     }
-
+    //segueway객체를 사용하기 때문에 prepare 메서드를 정의할 수 있고, 해당 메서드 내에서 다음 ViewController를 인스턴스화 할 수 있음
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let settingViewController = segue.destination as? SettingViewController{
+            settingViewController.delegate = self
+        }
+    }
 
 }
 
